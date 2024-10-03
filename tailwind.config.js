7966a2f1d5231c2dd4f71705682d7d80ec4fb6d0
DDEV User <nobody@example.com>
2024-10-03 12:07:27 +0000

    slider and carousel

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,24 @@ module.exports = {
   content: [
     "./templates/**/*.twig"
   ],
+  safelist: [
+    ...[...Array(10).keys()].flatMap(i => [`md:w-[${i*11}rem]`]),
+    ...[...Array(10).keys()].flatMap(i => [`w-[${i*8}rem]`]), 
+  ],
   theme: {
     extend: {
       fontFamily: {
-        body: "'Open Sans', ui-sans-serif",
-      }
+        display: "Roboto, ui-sans-serif",
+      },
+      animation: {
+        'slide-left': 'slide-left 24s linear infinite', 
+      },
+      keyframes: {
+        'slide-left': {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
     },
   },
   plugins: [],
